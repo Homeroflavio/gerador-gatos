60ffe0f1d9f70340c24a273c556796f79a5090d4
Homero Flávio Dubeux <homeroflavio8@gmail.com>
2026-03-30 16:54:52 -0300

    PWA

--- script.js
+++ script.js
@@ -9,6 +9,8 @@ function gerarGato(){
   img.src = "https://cataas.com/cat?" + new Date().getTime();
   img.classList.add("gato");
 
+  img.loading = "lazy"; // ✅ AQUI
+
   galeria.appendChild(img);
 
   contador++;
@@ -17,6 +19,10 @@ function gerarGato(){
   "Gatinhos gerados: " + contador;
 
   img.scrollIntoView({behavior:"smooth"});
+
+  if (navigator.vibrate) {
+    navigator.vibrate(100);
+  }
 }
 
 function limparGatos(){
@@ -28,3 +34,8 @@ function limparGatos(){
   document.getElementById("contador").innerText =
   "Gatinhos gerados: 0";
 }
+
+if ("serviceWorker" in navigator) {
+  navigator.serviceWorker.register("service-worker.js");
+}
+
